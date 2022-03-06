f59a42111860d75f5f2c9636bd064ee83d58dc07
Ashley's PC <ashlynn4567@gmail.com>
2022-03-06 11:34:06 -0600

    added HTML main structure

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+questionList = [
+    {
+        qustion: "Question 1",
+        choices: ["Answer 1", "Answer 2", "Answer 3"],
+        Answer: "Answer 2"
+    },
+    {
+        qustion: "Question 2",
+        choices: ["Answer 1", "Answer 2", "Answer 3"],
+        Answer: "Answer 2"
+    },
+    {
+        qustion: "Question 2",
+        choices: ["Answer 1", "Answer 2", "Answer 3"],
+        Answer: "Answer 2"
+    }
+];
